3712031723671adf528ad4c6ff5fdbf1f910d100
kpmarcin <73128255+kpmarcin@users.noreply.github.com>
2025-05-15 12:19:02 +0200

    Create zabbix_statistics.py
    Synchronize Zabbix Statistics to Sycope custom index

---
+++ zabbix_statistics.py
@@ -0,0 +1,447 @@
+## !/usr/bin/env python
+# coding: utf-8
+
+# Zabbix statistics integration with Sycope (Hostname, Response Time, CPU Load, Memory Usage, Packet Loss)
+# Script version: 1.0
+# Tested on Sycope 3.1
+
+import json
+from datetime import datetime, timedelta
+
+import polars as pl
+import requests
+from requests import Session
+
+# Hiding SSL certificate warning messages
+from urllib3.exceptions import InsecureRequestWarning
+
+requests.packages.urllib3.disable_warnings(category=InsecureRequestWarning)
+
+
+#### Script Parameters
+
+# Sycope credentials
+sycopeHost = "https://192.168.1.14"
+sycopeLogin = "admin"
+sycopePassword = ""
+
+# Zabbix credentials
+ZABBIX_URL = "http://192.168.1.46:8080/api_jsonrpc.php"
+ZABBIX_USER = "Admin"
+ZABBIX_PASSWORD = ""
+
+# Examples for IPs to be synchronized
+# The Zabbix API is quite fast, allowing you to synchronize all IPs;
+# however, it may be wise to consider limiting them for efficiency or control.
+TARGET_IPS = [
+    "8.8.8.8",
+    "192.168.1.1",    
+    "192.168.1.3",
+    "192.168.1.20",
+    "192.168.1.21",
+    "192.168.1.37",
+    "192.168.1.50",
+    "192.168.1.61",
+    "192.168.1.62",
+    "192.168.1.71",
+    "192.168.1.72",
+    "192.168.1.73"
+]
+
+# Custom Index name and history period
+INDEX_NAME = "ZabbixStats"
+HISTORY_PERIOD_MINUTES = 1440
+
+
+def val_numeric_range(val, lower=0, upper=100):
+    try:
+        return float(val) >= lower and float(val) <= upper
+    except Exception:
+        return False
+
+
+def val_div(x, div=100):
+    return x / div
+
+
+def val_multi(x, multi=1000):
+    return x * multi
+
+
+# Keys for metrics (multiple keys for CPU Load to support different platforms)
+# Other items can be included, but they would require additional columns in custom index
+ITEM_KEYS = {
+    "cpu_load": {
+        "values": ["system.cpu.load", "system.cpu.util"],
+        "output_type": float,
+        "validation_function": val_numeric_range,
+        "validation_args": {"lower": 0, "upper": 100},
+        "transform_function": val_div,
+        "round": 3,
+    },
+    "response_time": {
+        "values": ["icmppingsec"],
+        "output_type": float,
+        "validation_function": val_numeric_range,
+        "validation_args": {"lower": -1, "upper": 100000000},
+        "transform_function": val_multi,
+        "round": None,
+    },
+    "memory_usage": {
+        "values": ["vm.memory.util"],
+        "output_type": float,
+        "validation_function": val_numeric_range,
+        "validation_args": {"lower": 0, "upper": 100},
+        "transform_function": val_div,
+        "round": 3,
+    },
+    "packet_loss": {
+        "values": ["icmppingloss"],
+        "output_type": float,
+        "validation_function": val_numeric_range,
+        "validation_args": {"lower": 0, "upper": 100},
+        "transform_function": val_div,
+        "round": 3,
+    },
+}
+
+
+# Defining session for Sycope
+s = Session()
+
+
+def sycope_login():
+    payload = {"username": sycopeLogin, "password": sycopePassword}
+    r = s.post(sycopeHost + "/npm/api/v1/login", json=payload, verify=False)
+    data = r.json()
+
+    if data["status"] == 200:
+        print("Sycope API login successful. Proceeding...")
+    else:
+        # For debugging
+        print("Sycope API login failed:")
+        print(r.json())
+
+
+def sycope_logout():
+    r = s.get(sycopeHost + "/npm/api/v1/logout", verify=False)
+    s.close()
+
+
+def zabbix_login():
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "user.login",
+        "params": {"username": ZABBIX_USER, "password": ZABBIX_PASSWORD},
+        "id": 1,
+    }
+    response = requests.post(ZABBIX_URL, json=payload)
+    data = response.json()
+    if "result" in data:
+        print("Zabbix API login successful. Proceeding...")
+        return data["result"]
+    raise Exception(f"Zabbix API login failed: {data.get('error')}")
+
+
+def get_host_id_by_ip(auth_token, ip):
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "hostinterface.get",
+        "params": {"filter": {"ip": ip}},
+        "auth": auth_token,
+        "id": 2,
+    }
+    response = requests.post(ZABBIX_URL, json=payload)
+    data = response.json()["result"]
+    if data:
+        return data[0]["hostid"]
+    return None
+
+
+def get_hostname_by_id(auth_token, host_id):
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "host.get",
+        "params": {"output": ["hostid"], "selectInventory": ["name"], "hostids": host_id},
+        "auth": auth_token,
+        "id": 5,
+    }
+    response = requests.post(ZABBIX_URL, json=payload)
+    result = response.json()["result"]
+    # print(result)
+    if result and "inventory" in result[0] and result[0]["inventory"]:
+        return result[0]["inventory"].get("name", "")
+    return ""
+
+
+# def get_items_id(auth_token, host_id, key_):
+def get_items_id(auth_token, host_id):
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "item.get",
+        "params": {
+            "output": ["itemid", "name", "key_"],
+            "hostids": host_id,
+            # "search": {"key_": key_},
+            "sortfield": "name",
+        },
+        "auth": auth_token,
+        "id": 3,
+    }
+    response = requests.post(ZABBIX_URL, json=payload)
+
+    items = response.json()["result"]
+    if items:
+        return items
+    return []
+
+
+def get_history(auth_token, item_id, start_time, end_time, history_type=0):
+    payload = {
+        "jsonrpc": "2.0",
+        "method": "history.get",
+        "params": {
+            "output": "extend",
+            "history": history_type,
+            "itemids": item_id,
+            "sortfield": "clock",
+            "sortorder": "ASC",
+            "time_from": start_time,
+            "time_till": end_time,
+        },
+        "auth": auth_token,
+        "id": 4,
+    }
+
+    response = requests.post(ZABBIX_URL, json=payload)
+    return response.json()["result"]
+
+
+def create_minute_df_pl(start_datetime, stop_datetime):
+    return pl.DataFrame(
+        pl.datetime_range(
+            start=start_datetime,
+            end=stop_datetime,
+            interval="1m",
+            time_unit="ms",
+            eager=True,  # Return a Series directly
+        ).alias("datetime")
+    )
+
+
+def main():
+    auth_token = zabbix_login()
+
+    sycope_login()
+    columns = []
+
+    startTime = datetime.now() - timedelta(minutes=HISTORY_PERIOD_MINUTES)
+    endTime = datetime.now() - timedelta(seconds=75)
+    end_time_int = int(endTime.timestamp())
+    start_time_int = int(startTime.timestamp())
+    df_time_range = create_minute_df_pl(startTime.replace(second=30), endTime.replace(second=30))
+
+    dfs_ips = []
+    for ip in TARGET_IPS:
+        history_data = {}
+        for metric_name in ITEM_KEYS:
+            history_data[metric_name] = []
+        host_id = get_host_id_by_ip(auth_token, ip)
+        if not host_id:
+            continue
+
+        hostname = get_hostname_by_id(auth_token, host_id)
+        # items_id = get_items_id(auth_token, host_id, key_)
+        items_id = get_items_id(auth_token, host_id)
+        # items_ids_dict = {x["_key"].split("[")[0]:x["itemid"]}
+        items_ids_dict = {x["key_"].split("[")[0]: x["itemid"] for x in items_id}
+        # print(items_ids_dict)
+
+        for metric_name, val_config in ITEM_KEYS.items():
+            if not any([x in items_ids_dict for x in val_config["values"]]):
+                continue
+
+            for key_ in val_config["values"]:
+                if key_ not in items_ids_dict:
+                    continue
+                history = get_history(
+                    auth_token, items_ids_dict[key_], start_time_int, end_time_int, history_type=0
+                )
+
+                # For debugging
+                # print(history)
+                for entry in history:
+                    # For debugging
+                    # print(entry)
+                    timestamp = int(entry["clock"]) * 1000
+                    value = entry["value"]
+
+                    # For debugging
+                    # print(val_config)
+
+                    if val_config["validation_function"] and not val_config["validation_function"](
+                        value, **val_config["validation_args"]
+                    ):
+
+                        history_data[metric_name].append((timestamp, ip, hostname, None))
+                        continue
+
+                    value = val_config["output_type"](value)
+
+                    if val_config["transform_function"]:
+                        value = val_config["transform_function"](value)
+                    if val_config["output_type"] is float and val_config["round"]:
+                        value = round(value, val_config["round"])
+
+                    history_data[metric_name].append((timestamp, value))
+                    
+        if history_data:
+            dfs = []
+            for col, vals in history_data.items():
+                z = pl.DataFrame(vals, schema=["timestamp", col], orient="row").with_columns(
+                    datetime=pl.from_epoch(
+                        "timestamp", time_unit="ms"
+                    )
+                )
+                df = df_time_range.sort("datetime").join_asof(
+                    z.sort("datetime").drop("timestamp"), on="datetime", tolerance="59s", strategy="nearest"
+                )
+
+                dfs.append(df)
+            dfs_ips.append(
+                pl.concat(dfs, how="align_full").with_columns(ip=pl.lit(ip), hostname=pl.lit(hostname))
+            )
+
+
+    df = pl.DataFrame()
+    if dfs_ips:
+
+        df = pl.concat(dfs_ips, how="diagonal_relaxed")
+        df = df.with_columns(
+            timestamp=pl.col("datetime").dt.epoch(time_unit="ms"),
+            datetime=pl.col("datetime").dt.strftime("%Y/%m/%d %H:%M"),
+        )
+
+
+    ###### Checking current statistics in custom stream, in order not to duplicate samples
+
+    endTime = "@" + endTime.astimezone().isoformat("T", "seconds")
+    
+    # For debugging
+    # print(startTime)
+    # print("endTime =", endTime)
+
+    startTime = startTime.astimezone().isoformat("T", "seconds")
+    startTime = "@" + startTime
+
+    query = f'src stream="{INDEX_NAME}"'
+
+    ### Sending NQL query (HTTPS POST) to find ID of the requested IP address
+    ### Output will include jobId
+    off_size = 50000
+    payload = {
+        "startTime": startTime,
+        "endTime": endTime,
+        "nql": query,
+        "fsActive": False,
+        "waitTime": 30000,
+        "limit": off_size,
+    }
+
+    
+    r = s.post(sycopeHost + "/npm/api/v1/pipeline/run", json=payload, verify=False)
+    job_run = r.json()
+
+    # For debugging
+    # print(r.json())
+
+    ### Gathering query output using jobId
+    saved_data = []
+    for offset in range(job_run["data"]["total"] // off_size + 1):
+        # print(offset) # for debugging
+        payload = {"limit": off_size, "offset": offset * off_size}
+        r = s.post(f"{sycopeHost}/npm/api/v1/pipeline/{job_run['jobId']}/data", json=payload, verify=False)
+        chunk = r.json()
+        saved_data.extend(chunk["data"])
+
+    # For debugging
+    
+    # API response
+    # print("Data from Sycope:")
+    # print(json.dumps(saved_data))
+    # print("----------")
+
+    # print("Data from Zabbix:")
+    # print(json.dumps(new_data))
+
+    
+    if not df.is_empty():
+        print("We have found new data. Preparing the payload...")
+        if saved_data:
+            saved_df = pl.DataFrame(saved_data).with_columns(
+                datetime=pl.from_epoch(pl.col("timestamp"), time_unit="ms").dt.strftime("%Y/%m/%d %H:%M")
+            )
+
+            new_entries = df.join(saved_df, on=["datetime", "ip"], how="anti")
+
+        else:
+            new_entries = df
+        new_data = {
+            "columns": [
+                x
+                for x in [
+                    "timestamp",
+                    "ip",
+                    "hostname",
+                    "response_time",
+                    "cpu_load",
+                    "memory_usage",
+                    "packet_loss",
+                ]
+                if x in new_entries.columns
+            ],
+            "indexName": INDEX_NAME,
+            "sortTimestamp": True,
+            "rows": new_entries[
+                [
+                    x
+                    for x in [
+                        "timestamp",
+                        "ip",
+                        "hostname",
+                        "response_time",
+                        "cpu_load",
+                        "memory_usage",
+                        "packet_loss",
+                    ]
+                    if x in new_entries.columns
+                ]
+            ]
+            .drop_nulls(
+                subset=[
+                    x
+                    for x in ["response_time", "cpu_load", "memory_usage", "packet_loss"]
+                    if x in new_entries.columns
+                ]
+            )
+            .rows(),
+            
+        }
+        print("Sending new data to Sycope...")
+        r = s.post(sycopeHost + "/npm/api/v1/index/inject", json=new_data, verify=False)
+        data = r.json()
+        if data['status'] == 200:
+            print(f'Sycope API successfully saved new data.')
+        else:
+            #For debugging
+            print(f'Sycope API encountered an issue. Error message:')
+            print(r.json())
+        
+    # Closing the REST API session
+    # Session should be automatically closed in session context manager
+    print("Logging out from Sycope.")
+    sycope_logout()
+
+
+if __name__ == "__main__":
+    main()
